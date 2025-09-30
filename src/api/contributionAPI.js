c7e411de49f0e3c22310468df1ebc69490f0b1d6
nidhi500 <136151888+nidhi500@users.noreply.github.com>
2025-10-01 01:45:26 +0530

    Fix API base URL for deployment

--- src/api/contributionAPI.js
+++ src/api/contributionAPI.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000/api/contributions";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
 export const getContributions = (token) =>
   axios
--- src/api/monasteryAPI.js
+++ src/api/monasteryAPI.js
@@ -1,13 +1,13 @@
 import axios from "axios";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
-const API_URL = "http://localhost:5000/api/monasteries";
 
 // Fetch all monasteries
 export const getMonasteries = () =>
-  axios.get(API_URL).then(res => res.data);
+  axios.get(`${API_URL}/monasteries`).then(res => res.data);
 
 // Approve a monastery
 export const approveMonastery = (id, token) =>
   axios
-    .put(`${API_URL}/${id}/approve`, {}, { headers: { Authorization: `Bearer ${token}` } })
+    .put(`${API_URL}/monasteries/${id}/approve`, {}, { headers: { Authorization: `Bearer ${token}` } })
     .then(res => res.data);
